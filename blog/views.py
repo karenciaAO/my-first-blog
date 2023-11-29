1b0deaeb560626fc08a94754ec11235744698e9c
karenciaAO <karenaraqueo@gmail.com>
2023-11-29 17:09:08 -0500

    cambio base de datos
    base de datos Postgres en contenedor docker

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+#Como puedes ver, hemos creado una función 
+#(def) llamada post_list que acepta request 
+# y return una función render que reproduce 
+# (construye) nuestra plantilla blog/post_list.html.
